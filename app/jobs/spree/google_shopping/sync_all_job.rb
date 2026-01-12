44e77c52729ff83e201fe6b0b442e00db0d113a1
Umesh Ravani <umeshravani98@gmail.com>
2026-01-13 02:05:52 +0530

    Update sync_all_job.rb
    Instead of doing the work here, just dispatch 1000s of tiny jobs.
    so it takes seconds to run, releasing the worker immediately.

--- app/jobs/spree/google_shopping/sync_all_job.rb
+++ app/jobs/spree/google_shopping/sync_all_job.rb
@@ -1,18 +1,17 @@
 module Spree
-    module GoogleShopping
-      class SyncAllJob < SpreeGoogleProducts::BaseJob
-        #queue_as :default
-  
-        def perform
-          credential = Spree::Store.default.google_credential
-          return unless credential&.active?
-  
-          service = Spree::GoogleShopping::ContentService.new(credential)
+  module GoogleShopping
+    class SyncAllJob < SpreeGoogleProducts::BaseJob
+      
+      def perform
+        credential = Spree::Store.default.google_credential
+        return unless credential&.active?
           
-          Spree::Product.active.find_each(batch_size: 50) do |product|
-            service.push_product(product)
-          end
+        Spree::Product.active.find_each do |product|
+          Spree::GoogleShopping::SyncProductJob.perform_later(product.id)
         end
+        
+        Rails.logger.info "GOOGLE: Queued sync for #{Spree::Product.active.count} products."
       end
     end
   end
+end
