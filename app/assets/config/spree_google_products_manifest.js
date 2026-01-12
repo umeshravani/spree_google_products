bd8d2c5763d07de700fa82d573acd5e6f5d76b1c
Umesh Ravani <umeshravani98@gmail.com>
2026-01-12 17:52:31 +0000

    added styesheets

--- app/assets/config/spree_google_products_manifest.js
+++ app/assets/config/spree_google_products_manifest.js
@@ -3,3 +3,4 @@
 //= link_tree ../../javascript/spree_google_products/controllers .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../../../vendor/stylesheets .css
+//= link_directory ../stylesheets .css
