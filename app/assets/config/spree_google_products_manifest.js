ac5d433defda438af0cfbbf4d00997db5f573ad4
Umesh Ravani <umeshravani98@gmail.com>
2026-01-12 23:28:57 +0530

    Remove empty asset links

--- app/assets/config/spree_google_products_manifest.js
+++ app/assets/config/spree_google_products_manifest.js
@@ -1,6 +1,1 @@
-//= link_tree ../images
-//= link spree_google_products/application.js
-//= link_tree ../../javascript/spree_google_products/controllers .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../../../vendor/stylesheets .css
-//= link_directory ../stylesheets .css
+
