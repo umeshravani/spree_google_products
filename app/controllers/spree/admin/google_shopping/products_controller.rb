dab7792ee94fe2a6f41d71964c8f828424352419
Umesh Ravani <umeshravani98@gmail.com>
2026-02-12 13:30:52 +0530

    Controller Fixes for 5.3 compatibility
    FIX: 1. Removed Spree::Config[:admin_products_per_page] (It was deleted in Spree 5)
    2. We now default to 25 if no param is provided.

--- app/controllers/spree/admin/google_shopping/products_controller.rb
+++ app/controllers/spree/admin/google_shopping/products_controller.rb
@@ -1,21 +1,26 @@
+require 'kaminari'
+
 module Spree
   module Admin
     module GoogleShopping
       class ProductsController < Spree::Admin::ResourceController
 
-        def model_class
-          Spree::Product
-        end
-
         def index
-          per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 25
+          @per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 25
 
-          @search = Spree::Product.accessible_by(current_ability, :read).ransack(params[:q])
+          @search = collection.ransack(params[:q])
           
           @collection = @search.result(distinct: true)
+                               .includes(master: [:images, :default_price])
+                               .page(params[:page])
+                               .per(@per_page)
+        end
+
+        def collection
+          return @collection if @collection.present?
+          
+          @collection = Spree::Product.accessible_by(current_ability, :read)
                                       .includes(:google_product_attribute)
-                                      .page(params[:page])
-                                      .per(per_page)
         end
 
         def edit
@@ -25,34 +30,55 @@ module Spree
 
         def update
           @product = Spree::Product.friendly.find(params[:id])
+          
           if @product.update(product_params)
-
             Spree::GoogleShopping::SyncProductJob.perform_later(@product.id)
             
-            flash[:success] = "Product updated. Syncing to Google Merchant Center"
-            redirect_to admin_google_shopping_product_path(@product)
+            flash[:success] = "Product updated. Syncing to Google Merchant Center..."
+            redirect_to admin_google_shopping_products_path
           else
-            flash[:error] = "Could not update product"
+            flash[:error] = "Could not update product."
             render :edit
           end
         end
 
         def issues
           @product = Spree::Product.friendly.find(params[:id])
-          
           @variant = Spree::Variant.find_by(id: params[:variant_id])
           
           if @variant
-
             @google_attr = Spree::GoogleVariantAttribute.find_by(variant_id: @variant.id)
             @issues = @google_attr&.google_issues || []
           else
             flash[:error] = "Variant not found."
-            redirect_to admin_google_shopping_product_path(@product)
+            redirect_to admin_google_shopping_products_path
           end
         end
 
+        def sync
+          @product = Spree::Product.friendly.find(params[:id])
+          
+          if @product
+            Spree::GoogleShopping::SyncProductJob.perform_later(@product.id)
+            flash[:success] = "Sync queued for '#{@product.name}'."
+          else
+            flash[:error] = "Product not found."
+          end
+          
+          redirect_back(fallback_location: admin_google_shopping_products_path)
+        end
+
+        def sync_all
+           Spree::GoogleShopping::SyncAllJob.perform_later
+           flash[:success] = "Sync All background job started!"
+           redirect_back(fallback_location: admin_google_shopping_products_path)
+        end
+
         private
+
+        def model_class
+          Spree::Product
+        end
 
         def product_params
           params.require(:product).permit(
