dc3dd9c93a7b409fa3e4e4a57b8d474c3303642b
Umesh Ravani <umeshravani98@gmail.com>
2026-06-25 17:23:13 +0530

    Refactor Google Shopping sidebar initialization

--- config/initializers/spree_google_products.rb
+++ config/initializers/spree_google_products.rb
@@ -1,29 +1,40 @@
 Rails.application.config.after_initialize do
-    if Spree.respond_to?(:admin) && Spree.admin.respond_to?(:navigation)
-      sidebar = Spree.admin.navigation.sidebar
-      sidebar.add :google_shopping, 
-        label: 'Google Shopping', 
-        icon: 'brand-google', 
-        url: :admin_google_shopping_dashboard_path, 
-        position: 85
-      sidebar.add :google_dashboard, 
-        parent: :google_shopping, 
-        label: 'Dashboard', 
-        url: :admin_google_shopping_dashboard_path, 
-        active: -> { params[:controller].include?('google_shopping/dashboard') }
-      sidebar.add :google_products, 
-        parent: :google_shopping, 
-        label: 'Products', 
-        url: :admin_google_shopping_products_path, 
-        active: -> { params[:controller].include?('google_shopping/products') }
-      sidebar.add :google_settings, 
-        parent: :google_shopping, 
-        label: :settings, 
-        url: :edit_admin_google_merchant_settings_path, 
-        active: -> { controller_name == 'google_merchant_settings' }
-    end
-    
-    if defined?(Spree::Ability) && defined?(SpreeGoogleProducts::Ability)
+  if Spree.respond_to?(:admin) && Spree.admin.respond_to?(:navigation)
+    sidebar = Spree.admin.navigation.sidebar
+    sidebar.add :google_shopping, 
+      label: 'Google Shopping', 
+      icon: 'brand-google', 
+      url: :admin_google_shopping_dashboard_path, 
+      position: 85
+    sidebar.add :google_dashboard, 
+      parent: :google_shopping, 
+      label: 'Dashboard', 
+      url: :admin_google_shopping_dashboard_path, 
+      active: -> { params[:controller].include?('google_shopping/dashboard') }
+    sidebar.add :google_products, 
+      parent: :google_shopping, 
+      label: 'Products', 
+      url: :admin_google_shopping_products_path, 
+      active: -> { params[:controller].include?('google_shopping/products') }
+    sidebar.add :google_settings, 
+      parent: :google_shopping, 
+      label: :settings, 
+      url: :edit_admin_google_merchant_settings_path, 
+      active: -> { controller_name == 'google_merchant_settings' }
+  end
+  
+  if defined?(Spree::Ability) && defined?(SpreeGoogleProducts::Ability)
+    if Spree::Ability.respond_to?(:register_ability)
+      # Target: Spree 5.4
       Spree::Ability.register_ability(SpreeGoogleProducts::Ability)
+    else
+      # Target: Spree 5.5+
+      Spree::Ability.prepend(Module.new do
+        def initialize(user)
+          super
+          merge(SpreeGoogleProducts::Ability.new(user))
+        end
+      end)
     end
   end
+end
